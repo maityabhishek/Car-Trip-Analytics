b3a5e53a5d1f3d67949f282d55f233ab5bc0af8d
Abhishek Maity <a.maity@outlook.com>
2020-12-17 00:29:17 +0530

    Update TripAnalyticsService.java

--- src/main/java/com/cartripanalytics/service/TripAnalyticsService.java
+++ src/main/java/com/cartripanalytics/service/TripAnalyticsService.java
@@ -25,50 +25,42 @@ public class TripAnalyticsService {
 		double totalFuelConsumed=(list.get(0).getFuel())-(list.get(dataPointNos-1).getFuel());
 		double triptime=(list.get(dataPointNos-1).getTs())-(list.get(0).getTs());
 		double tripsplitkms=(totalKmstraveled/4.0);
-		double avgspeed=0.0;
 		
-		int fromKms=0;
-		double nextSplitpoint = tripsplitkms;
-		long odometerinitial=list.get(0).getOdometer();
-		int speed=0;
-		int splitDist=0;
-		
-		int dataCounter=0;
-		double initialfuel=list.get(0).getFuel();
-		double splitfuel=0.0;
+		double nextsplit=tripsplitkms;
+		int splitcount=1;
+		int sfromKm=0;
 		long initialTime=list.get(0).getTs();
 		long splitTime=0; 
-		List<TripSplits> splits=new ArrayList<TripSplits>();
+		int speed=0;
+		int datacount=1;
+		double initialfuel=list.get(0).getFuel();
+		double splitfuel=0.0;
 		
+		List<TripSplits> splits=new ArrayList<TripSplits>();
 		TripPoint tp=null;
 		for(int counter=1;counter<=list.size();counter++)
 		{
-			//System.out.println(list.get(counter-1));
 			tp=list.get(counter-1);
-			System.out.println(tp.getOdometer()-odometerinitial);
-			if(tp.getOdometer()>=nextSplitpoint|| tp.getOdometer()==list.get(dataPointNos-1).getOdometer())
+			System.out.println(tp);
+			if(tp.getOdometer()>=nextsplit)
 			{
-				
-				splits.add(new TripSplits(fromKms,splitDist,splitTime,speed/(dataCounter),splitfuel));
-				fromKms=splits.get(splits.size()-1).getToKms()+1;
-				dataCounter=0;
+				splitcount+=1;
+				splits.add(new TripSplits(sfromKm,(int) nextsplit,splitTime,speed/datacount,splitfuel));
+				sfromKm=(int) (nextsplit+1);
+				nextsplit=tripsplitkms*splitcount;
 				initialfuel=tp.getFuel();
-				initialTime=tp.getTs();
+				datacount=0;
 				speed=0;
-				System.out.println(splits.get(splits.size()-1));
-				nextSplitpoint=tripsplitkms*(splits.size()+1);
-				System.out.println(counter+"next on "+nextSplitpoint);
-				
+				System.out.println(splitcount+"\t\t"+nextsplit);
+				System.out.println("\n\t\twe Split next on "+nextsplit+"\t\tcur:"+tp.getOdometer()+ "\n\n");
 				
 			}
-			dataCounter++;
-			speed=speed+tp.getSpeed();
-			splitDist=(int) (tp.getOdometer()-odometerinitial);
-			splitfuel=initialfuel-tp.getFuel();
 			splitTime=tp.getTs()-initialTime;
-			//test
+			speed=speed+tp.getSpeed();
+			splitfuel=initialfuel-tp.getFuel();
+			datacount++;
 		}
-		avgspeed = (splits.stream().mapToInt(t -> t.getAvgSpeed()).sum())/4.0;
+		double avgspeed = (splits.stream().mapToInt(t -> t.getAvgSpeed()).sum())/4.0;
 		Trip trip=new Trip(1,"OD02F7497",totalKmstraveled,totalFuelConsumed,avgspeed,triptime,"A","B",new Date(list.get(0).getTs()),(int)Math.round(tripsplitkms),splits);
 		System.out.println("\n\n\n\n\n\n"+trip+"\n\n\n\n\n\n");
 
